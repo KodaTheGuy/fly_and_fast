ffa7a377894d2f68ebc368ae6d822b931e947b20
Koda_ <67247561+KodaTheGuy@users.noreply.github.com>
2020-11-19 19:26:41 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ func = function(name, param)
     local privs = minetest.get_player_privs(name)
     privs.fly = true
     minetest.set_player_privs(name, privs)
-    minetest.chat_send_all(minetest.colorize("FF00E0", name.." now has fly!"))
+    minetest.chat_send_all(minetest.colorize("#FF00E0", name.." now has fly!"))
 end,
 })
 
@@ -14,7 +14,7 @@ func = function(name, param)
     local privs = minetest.get_player_privs(name)
     privs.fast = true
     minetest.set_player_privs(name, privs)
-    minetest.chat_send_all(minetest.colorize("FF00E0", name.." now has fast!"))
+    minetest.chat_send_all(minetest.colorize("#FF00E0", name.." now has fast!"))
 end,
 })
 
